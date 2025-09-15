45b975130e50a6234d4652e14478a2316199bd0d
Andrea Giorni <andrea.giorni@avanade.com>
2025-09-15 18:31:32 +1000

    fixed OpenAI keys

--- tools.py
+++ tools.py
@@ -5,6 +5,11 @@ from langchain_community.document_loaders import WikipediaLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_openai import AzureOpenAIEmbeddings   
+import os
+from dotenv import load_dotenv  
+
+# load environment variables
+load_dotenv()  # take environment variables
 
 duckduck_tool = Tool(
     name="duckduckgo_search",
@@ -17,15 +22,12 @@ wikipedia = WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=3000)
 wikipedia_tool = WikipediaQueryRun(api_wrapper=wikipedia)
 
 
-
-
-
 embeddings = AzureOpenAIEmbeddings(
     model="text-embedding-3-large",
     # dimensions: Optional[int] = None, # Can specify dimensions with new text-embedding-3 models
-    # azure_endpoint="https://<your-endpoint>.openai.azure.com/", If not provided, will read env variable AZURE_OPENAI_ENDPOINT
-    # api_key=... # Can provide an API key directly. If missing read env variable AZURE_OPENAI_API_KEY
-    # openai_api_version=..., # If not provided, will read env variable AZURE_OPENAI_API_VERSION
+    azure_endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT"),
+    api_key = os.environ.get("AZURE_OPENAI_API_KEY"),
+    openai_api_version=os.environ.get("OPENAI_API_VERSION")
 )
 
 def wiki_RAG(query: str):
