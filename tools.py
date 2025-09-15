764669a5df1efea1939374abe012856e51b926f4
Andrea Giorni <andrea.giorni@avanade.com>
2025-09-15 15:25:58 +1000

    commit to branch

--- tools.py
+++ tools.py
@@ -1,9 +1,13 @@
-from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchRun, WikipediaQueryRun
 from langchain.tools import Tool
-
+from langchain_community.utilities import WikipediaAPIWrapper
 
 duckduck_tool = Tool(
     name="duckduckgo_search",
     func=DuckDuckGoSearchRun(),
     description="Searches DuckDuckGo for information from the web."
 )
+
+
+wikipedia = WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=300)
+wikipedia_tool = WikipediaQueryRun(api_wrapper=wikipedia)
